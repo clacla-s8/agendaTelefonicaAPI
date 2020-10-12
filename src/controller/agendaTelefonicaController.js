d5b5860a56d45a11071b3f02c7940a29ce1cc943
clacla-s8 <clariceks8@gmail.com>
2020-10-12 01:10:22 -0300

    metodos http implementados, GET, POST, PUT, PATCH e DELETE

--- src/controller/agendaTelefonicaController.js
+++ src/controller/agendaTelefonicaController.js
@@ -39,8 +39,71 @@ const criarContato = (req, res) => {
 }
 
 const atualizarContato = (req, res) => {
+    const { id } = req.params;
+    const { nome, telefone, email, outrosTelefone } = req.body;
+
+    localizarContato = agenda.findIndex(contato => contato.id == id);
+
+    if (localizarContato == -1) {
+        res.status(404).send({ mensagem: `Contato não encontrado` })
+    } else {
+        let contatoAtualizado = {
+            id: parseInt(id), // convertendo o id para int pois ele chega como string
+            nome: nome,
+            telefone: telefone,
+            email: email,
+            outrosTelefone: outrosTelefone
+        }
+
+        telefoneExiste = agenda.findIndex(contato => contato.telefone == telefone);
+
+
+        if (telefoneExiste == -1) {
+            agenda[localizarContato] = contatoAtualizado;
+            res.status(200).send({ mensagem: `Contato atualizado` })
+        } else {
+            res.status(400).send({ mensagem: `O telefone ${telefone} já existe` });
+        }
+    };
 
 }
+
+const atualizarTelefone = (req, res) => {
+    const { id } = req.params;
+    const { telefone } = req.body;
+
+    localizarContato = agenda.find(contato => contato.id == id);
+
+    if (localizarContato == null) {
+        res.status(404).send({ mensagem: `Contato não encontrado` })
+    } else {
+
+        telefoneExiste = agenda.findIndex(contato => contato.telefone == telefone);
+
+        if (telefoneExiste == -1) {
+            localizarContato.telefone = telefone;
+            res.status(200).send({ mensagem: `Contato atualizado` })
+        } else {
+            res.status(400).send({ mensagem: `O telefone ${telefone} já existe` });
+        }
+
+    }
+}
+
+const deletarContato = (req, res) => {
+    const { id } = req.params;
+
+    localizarContato = agenda.findIndex(contato => contato.id == id);
+
+    if (localizarContato == -1) {
+        res.status(404).send({ mensagem: `Contato não encontrado` })
+    } else {
+        agenda.splice(localizarContato, 1)
+        res.status(200).send({ mensagem: `Contado deletado com sucesso` })
+    }
+
+}
+
 
 
 module.exports = {
@@ -48,5 +111,7 @@ module.exports = {
     obterPorNome,
     obterPorTelefone,
     criarContato,
-    atualizarContato
+    atualizarContato,
+    atualizarTelefone,
+    deletarContato
 }
--- src/routes/router.js
+++ src/routes/router.js
@@ -2,12 +2,60 @@ const controller = require('../controller/agendaTelefonicaController');
 const express = require('express');
 const router = express.Router();
 
+/* 
+@router GET agenda
+@desc retorna todos os contatos da agenda
+@acess Public
+@endpoint http://localhost:porta/agenda/agenda
+*/
 router.get('/agenda', controller.obterContatos);
 
+/* 
+@router GET contato por nome
+@desc retorna o contato associado ao nome
+@acess Public
+@endpointhttp://localhost:porta/agenda/nome?nome=
+*/
 router.get('/nome', controller.obterPorNome);
 
+/* 
+@router GET contato por telefone
+@desc retorna o contato associado ao telefone
+@acess Public
+@endpointhttp://localhost:porta/agenda/telefone?telefone=
+*/
 router.get('/telefone', controller.obterPorTelefone);
 
+/* 
+@router POST criar
+@desc cria um novo contato e insere na agenda
+@acess Public
+@endpointhttp://localhost:porta/agenda/criar
+*/
 router.post('/criar', controller.criarContato);
+
+/* 
+@router PUT atualizar 
+@desc atualiza todas as informações do contato
+@acess Public
+@endpointhttp://localhost:porta/agenda/:id
+*/
+router.put('/:id', controller.atualizarContato);
+
+/* 
+@router PATCH atualizar 
+@desc atualiza o telefone do contato
+@acess Public
+@endpointhttp://localhost:porta/agenda/:id
+*/
+router.patch('/:id', controller.atualizarTelefone);
+
+/* 
+@router DELETE remover 
+@desc remove o contato pelo id
+@acess Public
+@endpointhttp://localhost:porta/agenda/:id
+*/
+router.delete('/:id', controller.deletarContato);
 
 module.exports = router;
